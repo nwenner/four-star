b466c3114d010806f8946f252a57fa4324a59e06
Nick Wenner <nwenner@gmail.com>
2020-12-30 23:27:25 -0600

    Added some conditional logic to the nav bar

--- src/navigationBar/navigationBar.js
+++ src/navigationBar/navigationBar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
 import { Hub, Auth } from "aws-amplify";
 
 import Navbar from 'react-bootstrap/Navbar';
@@ -11,37 +10,37 @@ function NavigationBar() {
 
     const [user, setUser] = useState(null);
     const [userGroups, setUserGroups] = useState(null);
-
+    
     useEffect(() => {
         Hub.listen("auth", ({ payload: { event, data } }) => {
-          switch (event) {
-            case "signIn":
-                getUser().then((userData) => setUser(userData));
-                break;
-            case "signOut":
-                setUser(null);
-                setUserGroups(null);
-                break;
-            case "signIn_failure":
-                console.log("Sign in failure", data);
-                break;
-            default:
-                break;
-          }
+            switch (event) {
+                case "signIn":
+                    getUser().then((userData) => setUser(userData));
+                    break;
+                case "signOut":
+                    setUser(null);
+                    setUserGroups(null);
+                    break;
+                case "signIn_failure":
+                    console.log("Sign in failure", data);
+                    break;
+                default:
+                    break;
+            }
         });
-    
+
         getUser().then((userData) => {
-          setUser(userData);
-          if (userData) {
-              console.log(`user: ${JSON.stringify(user)} `);
-            setUserGroups(
-              userData.signInUserSession.accessToken.payload["cognito:groups"]
-            );
-          } else {
-            setUserGroups(null);
-          }
+            setUser(userData);
+            if (userData) {
+                setUserGroups(
+                userData.signInUserSession.accessToken.payload["cognito:groups"]
+                );
+            } else {
+                setUserGroups(null);
+            }
         });
-      }, []);
+
+    }, []);
     
       function getUser() {
         return Auth.currentAuthenticatedUser()
@@ -58,10 +57,18 @@ function NavigationBar() {
             <Nav className="mr-auto">
                 <Nav.Link href="/main">Home</Nav.Link>
             </Nav>
-            <Form inline>
-                Group: {userGroups ? userGroups : null}
-                <span style={{marginRight: "5px"}}>Welcome <b>{user ? user.attributes.email : null}</b></span>
-                <a href="logout" className="Sign-out" onClick={() => Auth.signOut()}>Sign Out</a>
+            <Form inline className="Nav-form">
+                {user && 
+                    <div>
+                        <span style={{color: 'red', marginRight: "5px"}}><b>{userGroups ? 'Admin' : null}</b></span>
+                        <span style={{marginRight: "5px"}}>Welcome <b>{user ? user.attributes.email : null}</b></span>
+                        <a href="/main" className="Sign-out" onClick={() => Auth.signOut()}>Sign Out</a>
+                    </div>
+                }
+                {!user &&
+                    <a href="/login" className="Sign-out">Sign In</a>
+                }
+                
                 <FormControl type="text" placeholder="Filter" className="mr-sm-2" />
             </Form>
             </Navbar.Collapse>
