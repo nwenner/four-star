1eb97f782792f22434c91a6faadb45c96c27a09f
Nick Wenner <nwenner@gmail.com>
2020-12-30 22:55:28 -0600

    Added super basic authentication

--- src/authentication/login.js
+++ src/authentication/login.js
@@ -1,27 +1,61 @@
+import React, { useState } from "react";
+import { Auth } from "aws-amplify";
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Login() {
-  return (
-    <div className="App">
-        <header className="App-header">
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                <Form.Label>Please Login</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                Submit
-                </Button>
-            </Form>
-        </header>
+    const [email, setEmail] = useState();
+    const [pass, setPass] = useState();
+    const [errorMessage, setErrorMessage] = useState();
 
-    </div>
-  );
+    async function login(event) {
+        event.preventDefault();
+        try {
+            console.log('attempting to sign in');
+            let result = await Auth.signIn(email, pass);
+            if (result) {
+                console.log(result);
+                window.location.href = "/";
+            }
+        } catch (err) {
+        setErrorMessage(err.message);
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {errorMessage ? <p style={{ color: "red" }}>{errorMessage}</p> : null}
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Please Login</Form.Label>
+                        <Form.Control 
+                            type="email" 
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control 
+                            type="password" 
+                            placeholder="Password"
+                            onChange={(e) => setPass(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Button variant="primary" 
+                            type="submit" 
+                            onClick={login}>
+                    Log In
+                    </Button>
+                </Form>
+            </header>
+
+        </div>
+    );
 }
 
 export default Login;
--- src/navigationBar/navigationBar.js
+++ src/navigationBar/navigationBar.js
@@ -1,25 +1,72 @@
+import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+import { Hub, Auth } from "aws-amplify";
+
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 
 function NavigationBar() {
-  return (
-    <Navbar fixed="top" sticky="top" bg="dark" variant="dark">
-        <Navbar.Brand href="/main">
-          Four Star 
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/main">Home</Nav.Link>
-          </Nav>
-          <Form inline>
-            <a href="logout" className="Sign-out">Sign Out</a>
-            <FormControl type="text" placeholder="Filter" className="mr-sm-2" />
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-  );
+
+    const [user, setUser] = useState(null);
+    const [userGroups, setUserGroups] = useState(null);
+
+    useEffect(() => {
+        Hub.listen("auth", ({ payload: { event, data } }) => {
+          switch (event) {
+            case "signIn":
+                getUser().then((userData) => setUser(userData));
+                break;
+            case "signOut":
+                setUser(null);
+                setUserGroups(null);
+                break;
+            case "signIn_failure":
+                console.log("Sign in failure", data);
+                break;
+            default:
+                break;
+          }
+        });
+    
+        getUser().then((userData) => {
+          setUser(userData);
+          if (userData) {
+              console.log(`user: ${JSON.stringify(user)} `);
+            setUserGroups(
+              userData.signInUserSession.accessToken.payload["cognito:groups"]
+            );
+          } else {
+            setUserGroups(null);
+          }
+        });
+      }, []);
+    
+      function getUser() {
+        return Auth.currentAuthenticatedUser()
+          .then((userData) => userData)
+          .catch(() => console.log("Not signed in"));
+      }
+
+    return (
+        <Navbar fixed="top" sticky="top" bg="dark" variant="dark">
+            <Navbar.Brand href="/main">
+            Four Star 
+            </Navbar.Brand>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="/main">Home</Nav.Link>
+            </Nav>
+            <Form inline>
+                Group: {userGroups ? userGroups : null}
+                <span style={{marginRight: "5px"}}>Welcome <b>{user ? user.attributes.email : null}</b></span>
+                <a href="logout" className="Sign-out" onClick={() => Auth.signOut()}>Sign Out</a>
+                <FormControl type="text" placeholder="Filter" className="mr-sm-2" />
+            </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
 
 export default NavigationBar;
