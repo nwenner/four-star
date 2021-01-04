f489c12fca6fdd8b8d279a424762f546d10e5a34
Nick Wenner <nwenner@gmail.com>
2021-01-04 17:19:31 -0600

    Added delete comment when in admin mode

--- src/comments/comment.js
+++ src/comments/comment.js
@@ -1,17 +1,18 @@
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
 
 import { faStar, faThumbsDown, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import MovieRating from '../movieList/movieRating'
-import { useEffect } from "react";
+import {useAuth} from '../authentication/useAuth';
 
 function Comment(data) {
     let dateValue = new Date(data.comment.date).toLocaleString();
 
+    const auth = useAuth();
+
     return (
         <ListGroup.Item>
-            <div>
+            <div className="Comment-component-container">
                 <span>{dateValue}</span>
                 <div className="Comment-container">
                     <span>
@@ -28,8 +29,15 @@ function Comment(data) {
                     <FontAwesomeIcon icon={faStar} color={data.comment.rating >= 3.75 ? '#FFCC00' : ''}></FontAwesomeIcon>
                     <FontAwesomeIcon icon={faStar} color={data.comment.rating >= 4.75 ? '#FFCC00' : ''}></FontAwesomeIcon>
                 </div>
+                {auth.userGroups && 
+                    <div className="Comment-delete-container">
+                        <Button variant="warning" size="sm">Delete</Button>
+                    </div>
+                }
             </div>
-            <span><b>User: </b></span><span>{data.comment.username}</span>
+            <div>
+                <span><b>User: </b></span><span>{data.comment.username}</span>
+            </div>
             <div>{data.comment.comment}</div>
         </ListGroup.Item>
     );
